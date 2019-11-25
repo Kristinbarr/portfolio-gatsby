79c1bbb3b22048d3617e341e09630a5d4b40fec2
Kristin Barr <kristinbarr@gmail.com>
2019-11-25 02:01:26 -0500

    initial project

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Minim id cillum veniam mollit. Do in in mollit cillum et aliqua. Esse adipisicing sint dolor anim consectetur consectetur enim officia qui commodo consectetur exercitation commodo sint.</p>
+    </div>
+  )
+}
+
+export default AboutPage
