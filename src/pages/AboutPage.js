c4aff78d5d989049d782bd9097657a3aaee7159e
Kristin Barr <kristinbarr@gmail.com>
2019-12-15 23:55:23 -0500

    about page WIP

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const AboutPage = () => {
   return (
-    <div>
+    <Layout>
+      <SEO title="About Page" />
       <h1>About</h1>
-      <p>Minim id cillum veniam mollit. Do in in mollit cillum et aliqua. Esse adipisicing sint dolor anim consectetur consectetur enim officia qui commodo consectetur exercitation commodo sint.</p>
-    </div>
+      <p>
+        Minim id cillum veniam mollit. Do in in mollit cillum et aliqua. Esse
+        adipisicing sint dolor anim consectetur consectetur enim officia qui
+        commodo consectetur exercitation commodo sint.
+      </p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
   )
 }
 
