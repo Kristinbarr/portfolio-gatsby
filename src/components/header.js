480a2ab0f9d999860636fc313522226bb044bde9
Kristin Barr <kristinbarr@gmail.com>
2019-12-23 22:20:05 -0500

    header emotion comps and styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,28 @@
+import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import { HeaderContainer, HeaderInner, HeaderBar, HeaderH1 } from "../styles/headerStyles"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `greenYellow`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <HeaderContainer>
+    <HeaderBar />
+    <HeaderInner>
+      <HeaderH1>
         <Link
           to="/"
           style={{
-            color: `white`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
-      </h1>
-    </div>
-  </header>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/cv">CV</Link>
+        <Link to="/contact">Contact</Link>
+      </HeaderH1>
+    </HeaderInner>
+  </HeaderContainer>
 )
 
 Header.propTypes = {
