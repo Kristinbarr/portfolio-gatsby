589da80339f2ff9b5160f6fa7c58b1e09ca58f00
Kristin Barr <kristinbarr@gmail.com>
2019-12-31 16:23:24 -0500

    updated contact page - icon size and consolidation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,22 +9,20 @@ const ContactContainer = styled.div`
   justify-content: space-evenly;
   align-items: flex-end;
   flex-wrap: wrap;
-  padding-bottom: 0.5rem;
+  margin-bottom: 1rem;
 `
-const LinkWrapper = styled.div`
+const StyledLink = styled.a`
+  text-align: center;
+  text-decoration: none;
+  color: #4f4f4f;
   display: flex;
   justify-content: space-between;
   align-items: center;
   flex-direction: column;
   &:hover {
-    filter: invert(33%) sepia(92%) saturate(2693%) hue-rotate(258deg)
+    filter: invert(33%) sepia(92%) saturate(2700%) hue-rotate(238deg)
       brightness(101%) contrast(90%);
   }
-`
-const StyledLink = styled(Link)`
-  text-align: center;
-  text-decoration: none;
-  color: #4f4f4f;
   &:hover {
     text-shadow: 0 -15px 20px #000;
   }
@@ -33,7 +31,7 @@ const Title = styled.div`
   padding-top: 0.25rem;
 `
 const StyledImage = styled(Image)`
-  min-width: 1rem;
+  width: 1rem;
   text-align: center;
 `
 
@@ -47,7 +45,7 @@ const Contact = () => {
             url
             image {
               childImageSharp {
-                fluid(quality: 100) {
+                fluid(quality: 100, maxHeight: 40) {
                   ...GatsbyImageSharpFluid
                 }
               }
@@ -58,7 +56,7 @@ const Contact = () => {
     }
   `)
   const links = data.allContactJson.edges
-  console.log("links", links)
+  // console.log("links", links)
   return (
     <ContactContainer>
       {links.map(({ node: link }) => {
@@ -66,11 +64,9 @@ const Contact = () => {
         const url = link.url
         const imageData = link.image.childImageSharp.fluid
         return (
-          <StyledLink to={url}>
-          <LinkWrapper>
+          <StyledLink href={url} key={link.title}>
             <StyledImage fluid={imageData} />
             <Title>{title}</Title>
-          </LinkWrapper>
           </StyledLink>
         )
       })}
