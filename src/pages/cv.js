872b99aa253caddc1ba39b33f0b4cda0215e17e6
Kristin Barr <kristinbarr@gmail.com>
2019-12-31 16:21:28 -0500

    cv data and component updates

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -5,68 +5,95 @@ import styled from "@emotion/styled"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import StyledBackgroundSection from "../components/BackgroundSection"
-import CvPreview from "../components/CvPreview"
+import CvDownload from "../components/CvDownload"
 
-const H3 = styled.h3`
+const PageTitle = styled.h3`
   margin: 3rem 0 1.85rem 4.5rem;
 `
-const StyledSectionsWrapper = styled.div`
+const StyledWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+`
+const CvContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 80%;
+  padding: 1rem 1rem 2rem 1rem;
+  margin-bottom: 3rem;
+  background: #fff;
+  border: 2px solid #4f4f4f;
+  box-shadow: 4px 4px 0px #b24af2;
 `
 const StyledInnerSection = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  width: 80%;
-  padding: 1rem;
-  margin: 1rem;
-  text-align: center;
-  background: #fff;
-  border: 3px solid #4f4f4f;
-  box-shadow: 4px 4px 0px #b24af2;
-  transition: all 0.3s ease-in-out 0s;
-  &:hover {
-    box-shadow: 8px 8px 0px #b24af2;
-  }
+  width: 100%;
 `
-const CvContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 80%;
-  padding: 1rem;
-  margin: 1rem;
-  background: pink;
+const CvTitle = styled.div`
+  margin-top: 0.65rem;
 `
-const StyledWrapperTitle = styled.div`
-  width: 20rem;
-  margin: 1rem 1rem 1rem 0.5rem;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
-  transition: all 0.3s ease-in-out 0s;
-  &:hover:after {
-    // box-shadow: 0 0 8px rgba(0, 0, 0, 0.6);
-    // transform: scale(1.025, 1.025);
-    // background: rgba(0,0,0, .7);
-    // z-index: 5;
-    // content: "Go to live site";
-  }
-`
-const StyledWrapperSkills = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-const HGroup = styled.hgroup`
+const SkillsWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  font-size: 0.8rem;
+  line-height: 1rem;
+  text-align: center;
+  flex-wrap: wrap;
+  width: 90%;
 `
-const H4 = styled.h4``
-const H5 = styled.h5``
+const SkillSection = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  width: 30%;
+  flex-wrap: wrap;
+`
+const Skills = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+`
+const Skill = styled.div`
+  margin-right: 0.25rem;
+`
+const SectionWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  // flex-direction: column;
+  width: 90%;
+  text-align: center;
+  margin-bottom: .25rem;
+`
+const H4 = styled.h4`
+  font-weight: normal;
+  padding: 0 1rem 0.35rem 1rem;
+  border-bottom: 1px solid #e5e5e5;
+  margin-top: 1.25rem;
+  margin-bottom: .75rem;
+`
+const H5 = styled.h5`
+  margin-bottom: .35rem;
+  color: #4f4f4f;
+  font-weight: bold;
+`
 const H6 = styled.h6`
-  color: #aaa;
-  margin-bottom: 0.65rem;
+  font-weight: normal;
+  color: #949494;
+`
+const Span = styled.span`
+  margin-left: .5rem;
+  border-left: 1px solid #4f4f4f;
+  padding-left: .5rem;
+  font-weight: normal;
 `
 const Li = styled.li`
   font-size: 0.75rem;
@@ -77,86 +104,118 @@ const Li = styled.li`
 const CV = () => {
   const data = useStaticQuery(graphql`
     {
-      allCvJson {
-        edges {
-          node {
-            skills {
-              designer_skills
-              developer_skills
-              soft_skills
-            }
-            experience {
-              company
-              title
-              location
-              date
-              description
-            }
-            other_experience {
-              organization
-              role
-              location
-              date
-            }
-            education {
-              school
-              degree
-              location
-              date
-            }
-          }
+      cvJson {
+        skills_designer
+        skills_developer
+        skills_soft
+        experience {
+          company
+          title
+          location
+          date
+          description
+        }
+        other_experience {
+          organization
+          role
+          location
+          date
+        }
+        education {
+          school
+          degree
+          location
+          date
         }
       }
     }
   `)
 
-  const sections = data.allCvJson.edges
+  const sections = data.cvJson
 
   console.log("sections: ", sections)
-  console.log("sections skills: ", sections.node)
+  // console.log("sections single: ", sections.skills_designer)
 
   return (
     <Layout>
       <StyledBackgroundSection>
         <SEO title="CV" />
-        <H3>CV</H3>
-        <StyledSectionsWrapper>
-          <StyledInnerSection>
-            <CvContainer>
-              <StyledWrapperTitle>
-                {/* <Link to={`${url_live}`}>
-                  <StyledImage fluid={imageData} alt={title} />
-                </Link> */}
-              </StyledWrapperTitle>
-              <StyledWrapperSkills>
-                <H4>Skills</H4>
-                <ul>
-                  {/* <Li>{sections.node["skills"].designer_skills}</Li>
-                  <Li>{sections.node["skills"].developer_skills}</Li>
-                  <Li>{sections.node["skills"].soft_skills}</Li> */}
-                </ul>
-                <H4>Education</H4>
-                {/* {sections["experience"].map(({ node: job }) => (
-                  <>
-                    <HGroup>
-                      <H5>
-                        {job.company} | {job.title}
-                      </H5>
-                      <H6>
-                        {job.location} | {job.date}
-                      </H6>
-                    </HGroup>
-                    <ul>
-                      {job.description.map(duty => (
-                        <Li>- {duty}</Li>
-                      ))}
-                    </ul>
-                  </>
-                ))} */}
-              </StyledWrapperSkills>
-            </CvContainer>
-          </StyledInnerSection>
-        </StyledSectionsWrapper>
+        <PageTitle>CV</PageTitle>
+        <StyledWrapper>
+          <CvContainer>
+            <StyledInnerSection>
+              <CvTitle>
+                <h4>Kristin Barr</h4>
+              </CvTitle>
+
+              <H4>Skills</H4>
+              <SkillsWrapper>
+                <SkillSection>
+                  <H5>Designer</H5>
+                  <Skills>
+                    {sections.skills_designer.map(skill => (
+                      <Skill>{skill}</Skill>
+                    ))}
+                  </Skills>
+                </SkillSection>
+                <SkillSection>
+                  <H5>Developer</H5>
+                  <Skills>
+                    {sections.skills_developer.map(skill => (
+                      <Skill>{skill}</Skill>
+                    ))}
+                  </Skills>
+                </SkillSection>
+                <SkillSection>
+                  <H5>Soft</H5>
+                  <Skills>
+                    {sections.skills_soft.map(skill => (
+                      <Skill>{skill}</Skill>
+                    ))}
+                  </Skills>
+                </SkillSection>
+              </SkillsWrapper>
+
+              <H4>Experience</H4>
+              {sections.experience.map(job => (
+                <SectionWrapper>
+                  <H5>
+                    {job.company}
+                    <Span>{job.title}</Span>
+                  </H5>
+                  <H6>
+                    {job.location}, {job.date}
+                  </H6>
+                </SectionWrapper>
+              ))}
+              <H4>Other Experience</H4>
+              {sections.other_experience.map(job => (
+                <SectionWrapper>
+                  <H5>
+                    {job.organization}
+                    <Span>{job.role}</Span>
+                  </H5>
+                  <H6>
+                    {job.location}, {job.date}
+                  </H6>
+                </SectionWrapper>
+              ))}
+              <H4>Education</H4>
+              {sections.education.map(school => (
+                <SectionWrapper>
+                  <H5>
+                    {school.school}
+                    <Span>{school.degree}</Span>
+                  </H5>
+                  <H6>
+                    {school.location}, {school.date}
+                  </H6>
+                </SectionWrapper>
+              ))}
+            </StyledInnerSection>
+          </CvContainer>
+          <CvDownload />
+        </StyledWrapper>
       </StyledBackgroundSection>
     </Layout>
   )
