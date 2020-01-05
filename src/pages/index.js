d0dd8f3c39df87206bb2ce5e6a3e7dd3dc9626d7
Kristin Barr <kristinbarr@gmail.com>
2020-01-05 11:38:18 -0500

    home page resonsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,27 +6,44 @@ import SEO from "../components/SEO"
 import StyledBackgroundSection from "../components/BackgroundSection"
 import Headshot from "../components/Headshot"
 
+const PageTitle = styled.h3`
+  margin: 3rem 0 1.85rem 4.5rem;
+  @media (max-width: 576px) {
+    margin: 1.65rem 0 1.85rem 0;
+    text-align: center;
+  }
+`
 const StyledAboutWrapper = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+  @media (max-width: 576px) {
+    align-items: center;
+    flex-direction: column;
+  }
 `
 const StyledWrapperLeft = styled.div`
-  width: 40vw;
-  margin-bottom: 40vh;
-  font-size: .9rem;
+  width: 40%;
+  min-height: calc(100vh - 290px);
+  // background: green;
+  @media (max-width: 576px) {
+    min-height: 0;
+    margin-bottom: 1rem;
+    width: 80%;
+    text-align: justify;
+    text-justify: inter-character;
+  }
 `
 const StyledWrapperRight = styled.div`
   width: 25vw;
-  margin: 30vh 0 5rem 5vw;
+  margin: 10% 0 5rem 5%;
+  transition: all 0.3s ease-in-out 0s;
   &:hover {
     transform: rotate(-5deg);
   }
-  transition: all 0.3s ease-in-out 0s;
-  margin-top: 20%;
-`
-const PageTitle = styled.h3`
-  margin: 3rem 0 1.85rem 4.5rem;
+  @media (max-width: 576px) {
+    margin: 1rem 0 2rem 0;
+  }
 `
 
 const IndexPage = () => (
@@ -43,6 +60,9 @@ const IndexPage = () => (
             teams and enterprise clients, and team member on-boarding. Eager to
             power projects from concept to completion through teamwork, problem
             solving and thoughtful design.
+          </p>
+          <p>
+
           </p>
         </StyledWrapperLeft>
         <StyledWrapperRight>
